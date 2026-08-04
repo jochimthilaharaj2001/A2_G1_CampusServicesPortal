5460c4b8255b16757f6448318b0a0efbeda55d40
jochimthilaharaj2001 <bthilaharaj@gmail.com>
2026-08-04 20:59:36 +0530

    Campus service Portal Module 1 – Student Profile + Authentication

---
+++ campusBE/CampusServicePortal/Repositories/Interfaces/IAuthRepository.cs
@@ -0,0 +1,17 @@
+﻿using CampusServicePortal.Models;
+
+namespace CampusServicePortal.Repositories.Interfaces
+{
+    public interface IAuthRepository
+    {
+        Task<User?> GetUserByEmailAsync(string email);
+
+        Task<User?> GetUserByIdAsync(int userId);
+
+        Task AddUserAsync(User user);
+
+        Task AddStudentAsync(Student student);
+
+        Task SaveChangesAsync();
+    }
+}
